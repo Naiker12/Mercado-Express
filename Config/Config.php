7ba2afd5f36c8c6a5688c3fe48dd34b972973af1
Naiker Gomez <gomeznaiker@gmail.com>
2024-12-18 11:15:32 -0500

    Mi nuevo proyecto

---
+++ Config/Config.php
@@ -0,0 +1,15 @@
+<?php
+const BASE_URL = "http://localhost/Tienda-Virtual/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "Tienda-Virtual";
+const CHARSET = "charset=utf8";
+const TITLE = "Mercado Express";
+const MONEDA = "USD";
+const CLIENT_ID = "ID DE PASARELA DE PAGO CON PAYPAL";
+const USER_SMTP = "naikergomez0123@gmail.com";
+const PASS_SMTP = "ulwcodvnoselkmgc" ;
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com" ;
+?>
