0014a12814328fecaa22fd3d2d5857528a313b3c
Naiker Gomez <gomeznaiker@gmail.com>
2025-01-05 13:44:10 -0500

    Se agrego el controlador de Errors , y la pagina de error para mostrar un error cuando el usuario entre a un link no espesifico

---
+++ Views/errors/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <title>Error 404</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="FreshCart is a beautiful eCommerce HTML template specially designed for multipurpose shops & online stores selling products. Most Loved by Developers to build a store website easily.">
+<meta content="Codescandy" name="author" />
+
+
+<!-- Favicon icon-->
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo BASE_URL; ?>assets/img/favicon/favicon.ico">
+
+
+
+<!-- Libs CSS -->
+<link href="<?php echo BASE_URL; ?>assets/libs/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
+<link href="<?php echo BASE_URL; ?>assets/libs/feather-webfont/dist/feather-icons.css" rel="stylesheet" />
+<link href="<?php echo BASE_URL; ?>assets/libs/slick-carousel/slick/slick.css" rel="stylesheet" />
+<link href="<?php echo BASE_URL; ?>assets/libs/slick-carousel/slick/slick-theme.css" rel="stylesheet" />
+<link href="<?php echo BASE_URL; ?>assets/libs/simplebar/dist/simplebar.min.css" rel="stylesheet" />
+<link href="<?php echo BASE_URL; ?>assets/libs/nouislider/dist/nouislider.min.css" rel="stylesheet">
+<link href="<?php echo BASE_URL; ?>assets/libs/tiny-slider/dist/tiny-slider.css" rel="stylesheet">
+<link href="<?php echo BASE_URL; ?>assets/libs/dropzone/dist/min/dropzone.min.css" rel="stylesheet" />
+<link href="<?php echo BASE_URL; ?>assets/libs/prismjs/themes/prism-okaidia.min.css" rel="stylesheet">
+
+<!-- Theme CSS -->
+<link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/theme.min.css">
+</head>
+
+<body>
+<!-- section -->
+<section>
+    <div class="container d-flex flex-column">
+        <!-- fila -->
+        <div class="row min-vh-100 justify-content-center align-items-center">
+            <!-- columna -->
+            <div class="offset-lg-1 col-lg-10 py-8 py-xl-0">
+                <div class="mb-10 mb-xxl-0">
+                    <!-- imagen -->
+                    <a href="<?php echo BASE_URL; ?>">
+                        <img src="<?php echo BASE_URL; ?>assets/img/logo/freshcart-logo.svg" alt="">
+                    </a>
+                </div>
+                <div class="row justify-content-center align-items-center">
+                    <!-- contenido -->
+                    <div class="col-md-6">
+                        <div class="mb-6 mb-lg-0">
+                            <h1>Algo salió mal aquí...</h1>
+                            <p class="mb-8">No podemos encontrar la página que estás buscando.<br>
+                                Consulta nuestro centro de ayuda o vuelve a la página principal.</p>
+                            <!-- botón -->
+                            <a href="#" class="btn btn-dark">Centro de Ayuda <i class="feather-icon icon-arrow-right"></i></a>
+                            <!-- botón -->
+                            <a href="<?php echo BASE_URL; ?>" class="btn btn-primary ms-2">Volver a la página principal</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div>
+                            <!-- imagen -->
+                            <img src="<?php echo BASE_URL; ?>assets/img/svg-graphics/error.svg" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- section -->
+
+  <!-- Javascript-->
+  <!-- Libs JS -->
+<script src="<?php echo BASE_URL; ?>assets/libs/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/jquery-countdown/dist/jquery.countdown.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/slick-carousel/slick/slick.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/simplebar/dist/simplebar.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/nouislider/dist/nouislider.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/wnumb/wNumb.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/rater-js/index.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/prismjs/prism.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/prismjs/components/prism-scss.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/prismjs/plugins/toolbar/prism-toolbar.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/tiny-slider/dist/min/tiny-slider.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/dropzone/dist/min/dropzone.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/libs/flatpickr/dist/flatpickr.min.js"></script>
+
+<!-- Theme JS -->
+<script src="<?php echo BASE_URL; ?>assets/js/theme.min.js"></script>
+
+</body>
+</html>
